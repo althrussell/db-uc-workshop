23a9facb2787b9a347e8f6174195b97f451b5565
al.thrussell@databricks.com <al.thrussell@databricks.com>
2023-10-19 22:50:19 +0000

    db demos

---
+++ dbdemo-install.py
@@ -0,0 +1,26 @@
+# Databricks notebook source
+# MAGIC %pip install dbdemos
+
+# COMMAND ----------
+
+import dbdemos
+dbdemos.install('uc-05-upgrade')
+
+# COMMAND ----------
+
+import dbdemos
+dbdemos.install('uc-03-data-lineage')
+
+# COMMAND ----------
+
+import dbdemos
+dbdemos.install('uc-01-acl')
+
+# COMMAND ----------
+
+import dbdemos
+dbdemos.install('uc-02-external-location')
+
+# COMMAND ----------
+
+
