8b513a72078f999f9422cbaa6997a50fdc64fe8c
althrussell <althrussell>
2023-11-28 22:17:22 +0000

    lab 1 changes

--- _resources/set_params.py
+++ _resources/set_params.py
@@ -1,6 +1,7 @@
 # Databricks notebook source
 user_name = spark.sql("select current_user()").collect()[0][0].split("@")[0].replace(".","_").replace("+","_")
 source_db = f"hms_{user_name}"
+
 scale_factor = "10"
 tpcdi_directory = "s3://db-tpcdi-datagen/"
 files_directory = f"{tpcdi_directory}{scale_factor}"
@@ -8,6 +9,8 @@ files_directory = f"{tpcdi_directory}{scale_factor}"
 
 catalog = 'hive_metastore' #f"{dbutils.widgets.get('catalog')}"
 uc_catalog = f"uc_catalog_{user_name}"
+print("Your UC Catalog is : "+uc_catalog)
 uc_database = f"uc_db_{user_name}"
+print("Your UC Database is : "+uc_database)
 volume =  f"volume_{user_name}"
 
